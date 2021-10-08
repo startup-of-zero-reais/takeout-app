2c955a8c821d9db485f9a026430a3a28fa0de9ac
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-10-07 23:58:42 -0300

    removida pasta .idea

---
+++ src/pages/_app.tsx
@@ -0,0 +1,29 @@
+import type { AppProps } from 'next/app'
+import Head from "next/head";
+import { EmotionCache } from "@emotion/utils";
+import { CacheProvider } from "@emotion/react";
+import { CssBaseline, ThemeProvider } from "@material-ui/core";
+import { createEmotionCache } from '@src/pages/_document'
+import { theme } from "@src/styles/theme";
+import '../styles/globals.css'
+
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+	emotionCache?: EmotionCache
+}
+
+export default function MyApp( { Component, emotionCache = clientSideEmotionCache, pageProps }: MyAppProps ) {
+	return (
+		<CacheProvider value={ emotionCache }>
+			<Head>
+				<title>Home</title>
+				<meta name="viewport" content="initial-scale=1, width=device-width"/>
+			</Head>
+			<ThemeProvider theme={ theme }>
+				<CssBaseline/>
+				<Component { ...pageProps } />
+			</ThemeProvider>
+		</CacheProvider>
+	)
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+import { BottomNavigator } from "@src/components";
+
+const Home: NextPage = () => {
+	return (
+		<BottomNavigator/>
+	)
+}
+
+export default Home
