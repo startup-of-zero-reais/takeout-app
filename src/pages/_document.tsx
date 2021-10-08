87f0aba6be1245046b757fbce491a6370da58202
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-10-08 00:00:34 -0300

    configurados document e theme

---
+++ src/pages/_document.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+import createEmotionServer from '@emotion/server/create-instance'
+import createCache from '@emotion/cache';
+
+export function createEmotionCache() {
+	return createCache( { key: 'css' } )
+}
+
+class MyDoc extends Document {
+	render() {
+		return (
+			<Html lang='pt-br'>
+				<Head>
+					<link rel="stylesheet"
+					      href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
+					<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
+				</Head>
+				<body>
+				<Main/>
+				<NextScript/>
+				</body>
+			</Html>
+		)
+	}
+}
+
+MyDoc.getInitialProps = async ( ctx: DocumentContext ) => {
+	const originalRenderPage = ctx.renderPage;
+
+	const cache = createEmotionCache()
+	const { extractCriticalToChunks } = createEmotionServer( cache )
+
+	ctx.renderPage = () =>
+		originalRenderPage( {
+			// eslint-disable-next-line react/display-name
+			enhanceApp: ( App: any ) => ( props ) =>
+				<App emotionCache={ cache } { ...props } />,
+		} )
+
+	const initialProps = await Document.getInitialProps( ctx )
+	const emotionStyles = extractCriticalToChunks( initialProps.html )
+	const emotionStylesTags = emotionStyles.styles.map( style => (
+		<style
+			data-emotion={ `${ style.key } ${ style.ids.join( ' ' ) }` }
+			key={ style.key }
+			dangerouslySetInnerHTML={ { __html: style.css } }
+		/>
+	) )
+
+	return {
+		...initialProps,
+		styles: [ ...React.Children.toArray( initialProps.styles ), ...emotionStylesTags ]
+	}
+
+}
+
+export default MyDoc;
---
+++ src/styles/theme.ts
@@ -0,0 +1,20 @@
+import { createTheme } from "@material-ui/core";
+
+const colors = {
+	redVermilionBird: "#E92F48",
+	BlackTortoise: "#424242",
+	DeepGray: "#7E8389",
+	ToGoContainer: "#F2F2F2",
+	YellowDragon: "#FFC107",
+	SilverSycee: "#A0A8B1",
+	BronzeRitual: "#D97953",
+}
+
+export const customPalettes = ( k: keyof typeof colors ) => colors[ k ]
+
+export const theme = createTheme( {
+	palette: {
+		mode: 'light'
+	}
+} )
+
