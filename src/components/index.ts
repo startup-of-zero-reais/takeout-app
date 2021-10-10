4b22eeb397229b9b4a1966d6de450061cd975b00
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-10-10 04:19:16 -0300

    terminado layout de bottom navigator

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,3 @@
 export * from './layout/bottom-navigator'
+export * from './utils/render-if'
+export * from './utils/classnames'
--- src/components/layout/bottom-navigator/index.tsx
+++ src/components/layout/bottom-navigator/index.tsx
@@ -1,25 +1,97 @@
-import React, { useState } from 'react';
-import { BottomNavigation, BottomNavigationAction, Paper, ThemeProvider } from "@material-ui/core";
-import { HomeWorkSharp } from "@material-ui/icons";
-import { bottomNavigatorTheme } from "./custom-theme";
+import { useEffect, useRef, useState } from 'react';
+import _ from 'lodash';
+import { FiHome, FiShoppingCart, FiUser } from 'react-icons/fi';
+import { BsTrophy } from 'react-icons/bs';
+import styles from './style.module.scss';
+import { classnames } from "@src/components";
+import { BottomLink } from "./bottom-link";
 
 type BottomNavigatorProps = {}
 
 export const BottomNavigator = ( _: BottomNavigatorProps ) => {
-	const [ value, setValue ] = useState( 0 )
+	const [ win, setWin ] = useState<number>( 1024 );
+	const [ value, setValue ] = useState( 1 )
+
+	const onClick = ( v: number ) => ( e: any ) => {
+		e.preventDefault();
+		setValue( v )
+	}
+
+	useEffect( () => {
+		function listenResize( this: Window ) {
+			setWin( this.window.innerWidth )
+		}
+
+		addEventListener( 'resize', listenResize )
+
+		return () => {
+			removeEventListener( 'resize', listenResize )
+		}
+	}, [] )
 
 	return (
-		<ThemeProvider theme={ bottomNavigatorTheme }>
-			<Paper sx={ { position: 'fixed', bottom: 0, left: 0, right: 0 } } elevation={ 3 }>
-				<BottomNavigation
-					showLabels
-					value={ value }
-					onChange={ ( e, newValue ) => setValue( newValue ) }
-				>
-					<BottomNavigationAction label="Home" icon={ <HomeWorkSharp/> }/>
-					<BottomNavigationAction label="Cart" icon={ <HomeWorkSharp/> }/>
-				</BottomNavigation>
-			</Paper>
-		</ThemeProvider>
+		<nav className={ classnames( styles[ 'bottom-menu' ] ) }>
+			<div className={ classnames( styles[ 'menu' ] ) }>
+				<BottomLink
+					onClick={ onClick( 1 ) }
+					icon={ <FiHome size={ 24 }/> }
+					to={ "#" }
+					selected={ value === 1 }
+				/>
+
+				<BottomLink
+					onClick={ onClick( 2 ) }
+					icon={ <FiShoppingCart size={ 24 }/> }
+					to={ "#" }
+					selected={ value === 2 }
+				/>
+
+				<BottomLink
+					onClick={ onClick( 3 ) }
+					icon={ <BsTrophy size={ 24 }/> }
+					to={ "#" }
+					selected={ value === 3 }
+				/>
+
+				<BottomLink
+					onClick={ onClick( 4 ) }
+					icon={ <FiUser size={ 24 }/> }
+					to={ "#" }
+					selected={ value === 4 }
+				/>
+
+				<div className={ classnames( styles[ 'svg-animation' ], styles[ `column-${ value }` ] ) }>
+					{ genSvg( win ) }
+				</div>
+			</div>
+		</nav>
 	);
+}
+
+function genSvg( windowWidth: number = 1024 ) {
+	const size = _.clamp( windowWidth / 4, 40, 115 )
+	const width = size;
+	const height = size > 60 ? 60 : size;
+	const R = size / 4;
+
+	function makePath() {
+		const arc = ( x: number, y: number, reverse = false ) =>
+			`a ${ R } ${ R } 0 0 ${ reverse ? 0 : 1 } ${ x } ${ y }`
+
+		return [
+			`M 0 0`,
+			arc( R, R ),
+			arc( 2 * R, 0, true ),
+			arc( R, -R ),
+			`Z`
+		].join( ' ' )
+	}
+
+	const d = makePath()
+
+	return (
+		<svg width={ width } height={ height }>
+			<path d={ d } fill="white"/>
+		</svg>
+	)
 }
