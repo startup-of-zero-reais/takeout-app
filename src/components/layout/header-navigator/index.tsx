0bc28121a301d37b518b2390549094379d5d9527
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-10-11 00:23:38 -0300

    criado header navigator

---
+++ src/components/layout/header-navigator/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styles from './style.module.scss'
+import { IconType } from 'react-icons'
+import { classnames } from "@src/components";
+import { IconButton } from "@material-ui/core";
+import { useRouter } from "next/router";
+
+type HeaderNavigatorProps = {
+	title: string;
+	action: () => void;
+	icon: IconType;
+}
+
+export const HeaderNavigator = ( { title, icon: Icon, action }: HeaderNavigatorProps ) => {
+	const { back } = useRouter();
+
+	return (
+		<nav className={ classnames( styles[ 'header_top' ] ) }>
+			<IconButton onClick={ back } >
+				<Icon
+					size={ 18 }
+					color="#7E8389"
+					className={ classnames( styles[ 'header_icon' ] ) }
+				/>
+			</IconButton>
+
+			<h1>{ title }</h1>
+		</nav>
+	);
+}
