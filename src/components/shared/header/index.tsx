3ce7434a3e7b66461e24711360494853e5d305e5
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-10-11 00:25:52 -0300

    criado header compartilhado

---
+++ src/components/shared/header/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { FiArrowRight, FiShoppingBag } from 'react-icons/fi';
+import { classnames } from "@src/components";
+import styles from './styles.module.scss';
+
+export const Header = ( ) => {
+	return (
+		<div className={classnames(styles['header'])}>
+			<h1>
+				Entregando em <FiArrowRight size={18} /> Goiania
+			</h1>
+
+			<span>
+				<FiShoppingBag size={24} />
+			</span>
+		</div>
+	);
+}
