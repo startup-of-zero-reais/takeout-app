735951de99145c0a487180a5d3f2f7c68d3d6a57
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-10-18 21:02:47 -0300

    corrigido margin entre cards

--- src/components/pages/inicio/food-card/index.tsx
+++ src/components/pages/inicio/food-card/index.tsx
@@ -17,55 +17,57 @@ type FoodCardProps = {
 
 export const FoodCard = ( { slug, image, isOnBasket = false }: FoodCardProps ) => {
 	return (
-		<Link href={`/restaurante/${encodeURIComponent(slug)}`} passHref>
-			<a className={ classnames( styles[ 'food-card' ] ) }>
-				<div className={ classnames( styles[ 'food-image-wrapper' ] ) }>
-					<Image
-						src={ image }
-						alt={ "food-1" }
-						width={ 350 }
-						height={ 150 }
-						objectFit={ "cover" }
-						layout={ "responsive" }
-						loading={ "lazy" }
-						placeholder="blur"
-						blurDataURL={ blurDataURL() }
-					/>
-				</div>
-
-				<div className={ classnames( styles[ 'food-info' ] ) }>
-					<div className={ classnames( styles[ 'food-info-restaurant' ] ) }>
-						<h2>Katsuei { RenderIf( isOnBasket, (
-							<div className={ classnames( styles[ 'food-info-on-basket' ] ) }>
-								<FaShoppingBasket/>
-								<span>
-								<FaCheckCircle size={ 14 }/>
-							</span>
-							</div>
-						) ) }</h2>
-						<h3>
-							<span>Japanese</span>
-							<span>Sushi</span>
-						</h3>
-						<section>
-							<span>10-15 min</span>
-							<span><RiMotorbikeFill/> R$ 5,99</span>
-							<span><FiStar/> 4.8</span>
-						</section>
+		<div className={ classnames( styles[ 'food-card' ] ) }>
+			<Link href={ `/restaurante/${ encodeURIComponent( slug ) }` } passHref>
+				<a>
+					<div className={ classnames( styles[ 'food-image-wrapper' ] ) }>
+						<Image
+							src={ image }
+							alt={ "food-1" }
+							width={ 350 }
+							height={ 150 }
+							objectFit={ "cover" }
+							layout={ "responsive" }
+							loading={ "lazy" }
+							placeholder="blur"
+							blurDataURL={ blurDataURL() }
+						/>
 					</div>
 
-					{ RenderIf( '/food-2.jpg' === image, (
-						<div className={ classnames( styles[ 'food-info-ranking' ] ) }>
-							<AvatarGroup max={ 3 }>
-								<AvatarWithBadge position={ 1 }/>
-								<AvatarWithBadge position={ 2 }/>
-								<AvatarWithBadge position={ 3 }/>
-							</AvatarGroup>
+					<div className={ classnames( styles[ 'food-info' ] ) }>
+						<div className={ classnames( styles[ 'food-info-restaurant' ] ) }>
+							<h2>Katsuei { RenderIf( isOnBasket, (
+								<div className={ classnames( styles[ 'food-info-on-basket' ] ) }>
+									<FaShoppingBasket/>
+									<span>
+								<FaCheckCircle size={ 14 }/>
+							</span>
+								</div>
+							) ) }</h2>
+							<h3>
+								<span>Japanese</span>
+								<span>Sushi</span>
+							</h3>
+							<section>
+								<span>10-15 min</span>
+								<span><RiMotorbikeFill/> R$ 5,99</span>
+								<span><FiStar/> 4.8</span>
+							</section>
 						</div>
-					) ) }
-				</div>
-			</a>
-		</Link>
+
+						{ RenderIf( '/food-2.jpg' === image, (
+							<div className={ classnames( styles[ 'food-info-ranking' ] ) }>
+								<AvatarGroup max={ 3 }>
+									<AvatarWithBadge position={ 1 }/>
+									<AvatarWithBadge position={ 2 }/>
+									<AvatarWithBadge position={ 3 }/>
+								</AvatarGroup>
+							</div>
+						) ) }
+					</div>
+				</a>
+			</Link>
+		</div>
 	);
 }
 
