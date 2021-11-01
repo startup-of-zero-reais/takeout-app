620a3c8dd1b633abed24108a00bc0b926828f822
Jean <jean.carlosmolossi@gmail.com>
2021-11-01 18:13:38 -0300

    remove da tela o modal caso esteja fechado

--- src/components/shared/modal/no-ssr.tsx
+++ src/components/shared/modal/no-ssr.tsx
@@ -1,11 +1,12 @@
 import { ComponentType } from "react";
 import ReactDOM from 'react-dom';
 import dynamic from "next/dynamic";
+import { RenderIf } from "@src/components";
 
-export function noSsrModal<T>( Component: ComponentType<T> ): ComponentType<T> {
+export function noSsrModal<T extends { open: boolean }>( Component: ComponentType<T> ): ComponentType<T> {
 	return dynamic(() => Promise.resolve(( props: T ) => {
 		return ReactDOM.createPortal(
-			<Component { ...props }/>,
+			RenderIf(props.open, <Component { ...props }/>),
 			document.getElementById("modals-wrapper") ?? document.body
 		)
 	}), { ssr: false });
