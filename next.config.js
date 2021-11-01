7949986820f5b246e9daaf6d4474b51fccf2f587
Jean <jean.carlosmolossi@gmail.com>
2021-11-01 17:17:58 -0300

    adicionado paths de styles

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const path = require('path');
 module.exports = {
     reactStrictMode: true,
     sassOptions: {
-        includePaths: [path.join(__dirname, 'src')],
+        includePaths: [
+            path.join(__dirname, 'src', 'styles'),
+            path.join(__dirname, 'src', 'pages', '**'),
+            path.join(__dirname, 'src', 'components', '**', '**'),
+        ],
     },
 }
