803a4bdb79fffffb17870af4e16a0c8ae1d3bff0
Jean <jean.carlosmolossi@gmail.com>
2021-11-06 13:45:55 -0300

    melhorado modal de cupom

--- src/pages/carrinho/index.tsx
+++ src/pages/carrinho/index.tsx
@@ -1,98 +1,113 @@
 import React from 'react';
 import {
-	classnames,
-	CouponModal,
-	HeaderNavigator,
-	MainGrid,
-	useModal
+		classnames,
+		CouponModal,
+		HeaderNavigator,
+		MainGrid, RenderIf,
+		useModal
 } from "@src/components";
 import { IoArrowBackSharp } from "react-icons/io5";
+import { FiXCircle } from "react-icons/fi";
 import { HiPlus } from "react-icons/hi";
 import { BsBox } from "react-icons/bs";
 import { Button } from "@mui/material";
 import styles from "./styles.module.scss";
 
 const Cart = () => {
-	const [ open, onOpen, onClose ] = useModal();
+		const [ open, onOpen, onClose ] = useModal();
 
-	return (
-		<MainGrid>
-			<HeaderNavigator
-				title="Carrinho"
-				icon={ IoArrowBackSharp }
-				action={ console.log }
-			/>
+		return (
+				<MainGrid>
+						<HeaderNavigator
+								title="Carrinho"
+								icon={ IoArrowBackSharp }
+								action={ console.log }
+						/>
 
-			<main className={ classnames(styles['container_cart']) }>
-				<div className={ classnames(styles['header_content_cart']) }>
-					<h1>Seu pedido</h1>
-					<Button variant={ 'text' } endIcon={ <HiPlus size={ 14 }/> }>
-						<span>Adicionar item</span>
-					</Button>
-				</div>
+						<main className={ classnames(styles.containerCart) }>
+								<div className={ classnames(styles.headerContentCart) }>
+										<h1>Seu pedido</h1>
+										<Button variant={ 'text' } endIcon={ <HiPlus size={ 14 }/> }>
+												<span>Adicionar item</span>
+										</Button>
+								</div>
 
-				{ [ 1, 2, 3 ].map(( _, k ) => (
-					<div
-						key={ k.toString() }
-						className={ classnames(styles['products_cart'], styles['products_cart']) }
-					>
-						<div className={ classnames(styles['products_info']) }>
-							<h3 className={ classnames(styles['bubble_number']) }>
-								1
-							</h3>
-							<div>
-								<h4>California Roll</h4>
-								<span>crab & Cucumber</span>
-							</div>
-						</div>
-						<span className={ classnames(styles['product_price']) }>R$ 4,99</span>
-					</div>
-				)) }
+								{ [ 1, 2, 3 ].map(( _, k ) => (
+										<div
+												key={ k.toString() }
+												className={ classnames(styles.productsCart) }
+										>
+												<div className={ classnames(styles.productsInfo) }>
+														<h3 className={ classnames(styles.bubbleNumber) }>
+																1
+														</h3>
+														<div>
+																<h4>California Roll</h4>
+																<span>crab & Cucumber</span>
+														</div>
+												</div>
+												<span className={ classnames(styles.productPrice) }>R$ 4,99</span>
+										</div>
+								)) }
 
-				<div className={ classnames(styles['container_summary']) }>
-					<span className={ classnames(styles['title_summary']) }>
+								<div className={ classnames(styles.containerSummary) }>
+					<span className={ classnames(styles.titleSummary) }>
 						<h3>Resumo</h3>
 				    </span>
 
-					<Button
-						startIcon={ <BsBox size={ 20 } color={ '#7E8389' }/> }
-						className={ classnames(styles['credit']) }
-						onClick={ onOpen }
-					>
-						<b>Aplicar cupom de desconto</b>
-						<span>R$ 5,00</span>
-					</Button>
-				</div>
+										<Button
+												startIcon={ <BsBox size={ 20 } color={ '#7E8389' }/> }
+												className={ classnames(styles.credit) }
+												onClick={ onOpen }
+										>
+												<b>Aplicar cupom de desconto</b>
+												<span>R$ 5,00</span>
+										</Button>
+								</div>
 
-				<div className={ classnames(styles['product_price_total']) }>
-					<div className={ classnames(styles['product_price_result'], styles['after_content']) }>
-						<span>Subtotal</span>
-						<p>R$ 14,97</p>
-					</div>
+								<div className={ classnames(styles.productPriceTotal) }>
+										<div className={ classnames(styles.productPriceResult) }>
+												<span>Subtotal</span>
+												<p>R$ 14,97</p>
+										</div>
 
-					<div className={ classnames(styles['product_price_result'], styles['after_content']) }>
-						<span>Delivery Free</span>
-						<p>R$ 14,97</p>
-					</div>
+										<div className={ classnames(styles.productPriceResult) }>
+												<span>Delivery Free</span>
+												<p>R$ 14,97</p>
+										</div>
 
-					<div className={ classnames(styles['product_price_result'], styles['after_content']) }>
-						<h4>Total</h4>
-						<p>R$ 14,97</p>
-					</div>
-				</div>
+										{ RenderIf(true, (
+												<div className={ classnames(styles.appliedCoupon) }>
+														<div>
+																<span>Cupom aplicado</span>
+																<span>- R$ 5,00</span>
+														</div>
 
-				<Button
-					variant="contained"
-					color="primary"
-					className={ classnames(styles['button_checkout']) }
-				>
-					Finalizar pedido
-				</Button>
-			</main>
+														<button>
+																<FiXCircle/>
+																Remover cupom
+														</button>
+												</div>
+										)) }
 
-			<CouponModal open={ open } onClose={ onClose }/>
-		</MainGrid>
-	);
+										<div className={ classnames(styles.productPriceResult) }>
+												<h4>Total</h4>
+												<p>R$ 14,97</p>
+										</div>
+								</div>
+
+								<Button
+										variant="contained"
+										color="primary"
+										className={ classnames(styles.buttonCheckout) }
+								>
+										Finalizar pedido
+								</Button>
+						</main>
+
+						<CouponModal open={ open } onClose={ onClose }/>
+				</MainGrid>
+		);
 }
 
 export default Cart;
