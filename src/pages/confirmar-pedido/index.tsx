669ac5af949f1ee6f1baf460798e68190ff32977
Jean <jean.carlosmolossi@gmail.com>
2021-11-06 13:54:46 -0300

    criada pagina de confirmar pedido

---
+++ src/pages/confirmar-pedido/index.tsx
@@ -0,0 +1,12 @@
+import { HeaderNavigator, MainGrid } from "@src/components";
+
+const ConfirmOrder = () => {
+		return (
+				<MainGrid>
+						<HeaderNavigator title={ "Confirmar pedido" }/>
+						<h1>Confirmar pedido</h1>
+				</MainGrid>
+		)
+}
+
+export default ConfirmOrder;
