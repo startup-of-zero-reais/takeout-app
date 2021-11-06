25e0cd8b7ea69784114a818b4184e1dabb3669b2
Jean <jean.carlosmolossi@gmail.com>
2021-11-06 13:54:08 -0300

    header navigator refatorado

--- src/components/layout/header-navigator/index.tsx
+++ src/components/layout/header-navigator/index.tsx
@@ -1,30 +1,30 @@
 import React from 'react';
-import styles from './style.module.scss'
 import { IconType } from 'react-icons'
-import { classnames } from "@src/components";
+import { IoArrowBackSharp } from 'react-icons/io5';
 import { IconButton } from "@mui/material";
 import { useRouter } from "next/router";
+import { classnames } from "@src/components";
+import styles from './style.module.scss'
 
 type HeaderNavigatorProps = {
-	title: string;
-	action: () => void;
-	icon: IconType;
+		title: string;
+		icon?: IconType;
 }
 
-export const HeaderNavigator = ( { title, icon: Icon, action: _ }: HeaderNavigatorProps ) => {
-	const { back } = useRouter();
+export const HeaderNavigator = ( { title, icon: Icon = IoArrowBackSharp }: HeaderNavigatorProps ) => {
+		const { back } = useRouter();
 
-	return (
-		<nav className={ classnames(styles[ 'header_top' ]) }>
-			<IconButton onClick={ back }>
-				<Icon
-					size={ 18 }
-					color="#7E8389"
-					className={ classnames(styles[ 'header_icon' ]) }
-				/>
-			</IconButton>
+		return (
+				<nav className={ classnames(styles['header_top']) }>
+						<IconButton onClick={ back }>
+								<Icon
+										size={ 18 }
+										color="#7E8389"
+										className={ classnames(styles['header_icon']) }
+								/>
+						</IconButton>
 
-			<h1>{ title }</h1>
-		</nav>
-	);
+						<h1>{ title }</h1>
+				</nav>
+		);
 }
