552e8ea7c1ba9bbb3051226f9c3c6137a98d927d
Jean <jean.carlosmolossi@gmail.com>
2021-11-07 20:16:32 -0300

    ajustes de main grid

--- src/components/layout/main-grid/index.tsx
+++ src/components/layout/main-grid/index.tsx
@@ -3,13 +3,14 @@ import styles from './styles.module.scss';
 import { classnames } from "@src/components";
 
 type MainGridProps = {
-	children?: ReactNode;
+		children?: ReactNode;
+		className?: string;
 }
 
-export const MainGrid = ( { children }: MainGridProps ) => {
-	return (
-		<div className={ classnames(styles[ 'main-grid' ]) }>
-			{ children }
-		</div>
-	);
+export const MainGrid = ( { children, className = '' }: MainGridProps ) => {
+		return (
+				<div className={ classnames(styles['main-grid'], className) }>
+						{ children }
+				</div>
+		);
 }
