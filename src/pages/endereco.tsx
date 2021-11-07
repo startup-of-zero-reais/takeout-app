31b9d15d9d741e631598dc61df5e1f580bcd0c2e
Jean <jean.carlosmolossi@gmail.com>
2021-11-07 20:15:52 -0300

    ajustes na pagina de endereco.tsx

--- src/pages/endereco.tsx
+++ src/pages/endereco.tsx
@@ -1,96 +1,93 @@
 import React from 'react';
-import { classnames, HeaderNavigator, MainGrid } from "@src/components";
-import { IoArrowBackSharp } from "react-icons/io5";
-import styles from "@src/components/pages/endereco/styles.module.scss";
 import { IconButton, InputAdornment, TextField } from "@mui/material";
 import { FiMic, FiSearch } from "react-icons/fi";
 import { TiLocationArrow } from "react-icons/ti";
 import { ImLocation } from "react-icons/im";
+import { classnames, HeaderNavigator, MainGrid } from "@src/components";
+import styles from "@src/components/pages/endereco/styles.module.scss";
 
 type EnderecoProps = {}
 
 const Endereco = ( _: EnderecoProps ) => {
-	return (
-		<MainGrid>
-			<HeaderNavigator
-				title="Endereço"
-				icon={ IoArrowBackSharp }
-				action={ console.log }
-			/>
+		return (
+				<MainGrid>
+						<HeaderNavigator
+								title="Endereço"
+						/>
 
-			<div className={ classnames(styles[ 'search-bar' ]) }>
-				<TextField
-					placeholder={ "Adicionar endereço" }
-					variant={ "outlined" }
-					InputProps={ {
-						startAdornment: (
-							<InputAdornment position={ "start" }>
-								<IconButton>
-									<FiSearch size={ 18 } />
-								</IconButton>
-							</InputAdornment>
-						),
-						endAdornment: (
-							<InputAdornment position={ "end" }>
-								<IconButton>
-									<FiMic size={ 18 } />
-								</IconButton>
-							</InputAdornment>
-						)
-					} }
-				/>
-			</div>
-
-			<div className={ classnames(styles[ 'locations' ]) }>
-				{ locations.map(( location ) => (
-					<button key={ location.id } className={ classnames(styles[ 'single-location' ]) }>
-						<div>
-							{ location.isCurrentLocation
-								? <TiLocationArrow size={ 32 } />
-								: <ImLocation size={ 28 } />
-							}
+						<div className={ classnames(styles['search-bar']) }>
+								<TextField
+										placeholder={ "Adicionar endereço" }
+										variant={ "outlined" }
+										InputProps={ {
+												startAdornment: (
+														<InputAdornment position={ "start" }>
+																<IconButton>
+																		<FiSearch size={ 18 }/>
+																</IconButton>
+														</InputAdornment>
+												),
+												endAdornment: (
+														<InputAdornment position={ "end" }>
+																<IconButton>
+																		<FiMic size={ 18 }/>
+																</IconButton>
+														</InputAdornment>
+												)
+										} }
+								/>
 						</div>
 
-						<div>
-							<h3>
-								{ location.isCurrentLocation
-									? 'Localização atual'
-									: location.name
-								}
-							</h3>
-							<span>
+						<div className={ classnames(styles['locations']) }>
+								{ locations.map(( location ) => (
+										<button key={ location.id } className={ classnames(styles['single-location']) }>
+												<div>
+														{ location.isCurrentLocation
+																? <TiLocationArrow size={ 32 }/>
+																: <ImLocation size={ 28 }/>
+														}
+												</div>
+
+												<div>
+														<h3>
+																{ location.isCurrentLocation
+																		? 'Localização atual'
+																		: location.name
+																}
+														</h3>
+														<span>
 								{ location.location }
 							</span>
+												</div>
+										</button>
+								)) }
 						</div>
-					</button>
-				)) }
-			</div>
-		</MainGrid>
-	);
+				</MainGrid>
+		);
 }
 
 const locations = [
-	{
-		id: '1234-abcd-5678-efgh',
-		name: 'Casa do John',
-		isCurrentLocation: true,
-		default: false,
-		location: '200 Eastern Pkwy, Brooklyn, NY 11238'
-	},
-	{
-		id: '1234-abcd-5678-efgi',
-		name: 'Minha casa',
-		isCurrentLocation: false,
-		default: true,
-		location: '29 Norman Ave, Brooklyn, NY 11222'
-	},
-	{
-		id: '1234-abcd-5678-efgj',
-		name: 'Trampo',
-		isCurrentLocation: false,
-		default: false,
-		location: '434 Broadway Floor 3, New York, NY 10013'
-	},
+		{
+				id: '1234-abcd-5678-efgh',
+				name: 'Casa do John',
+				isCurrentLocation: true,
+				default: false,
+				location: '200 Eastern Pkwy, Brooklyn, NY 11238'
+		},
+		{
+				id: '1234-abcd-5678-efgi',
+				name: 'Minha casa',
+				isCurrentLocation: false,
+				default: true,
+				location: '29 Norman Ave, Brooklyn, NY 11222'
+		},
+		{
+				id: '1234-abcd-5678-efgj',
+				name: 'Trampo',
+				isCurrentLocation: false,
+				default: false,
+				location: '434 Broadway Floor 3, New York, NY 10013'
+		},
 ]
 
 export default Endereco
