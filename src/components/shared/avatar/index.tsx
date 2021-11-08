63e4c4ca53dcb5da3c3fda3a82a19ebf58ebe625
Jean <jean.carlosmolossi@gmail.com>
2021-11-07 23:11:10 -0300

    adicionada propriedade noBadge

--- src/components/shared/avatar/index.tsx
+++ src/components/shared/avatar/index.tsx
@@ -6,78 +6,80 @@ import styles from './styles.module.scss';
 type BadgeType = 'simple' | 'custom';
 
 type AnchorPoints = {
-	horizontal: 'left' | 'center' | 'right';
-	vertical: 'top' | 'middle' | 'bottom';
+		horizontal: 'left' | 'center' | 'right';
+		vertical: 'top' | 'middle' | 'bottom';
 }
 
 type AvatarBadge = {
-	model?: BadgeType;
-	customComponent?: ReactNode;
-	anchorPoints?: AnchorPoints;
-	className?: string;
+		model?: BadgeType;
+		customComponent?: ReactNode;
+		anchorPoints?: AnchorPoints;
+		className?: string;
 }
 
 type AvatarProps = {
-	alt?: string;
-	src: string;
-	size?: number;
-	wrapperClassName?: string;
-	imageClassName?: string
-	badge?: AvatarBadge;
+		alt?: string;
+		src: string;
+		size?: number;
+		wrapperClassName?: string;
+		imageClassName?: string
+		badge?: AvatarBadge;
+		noBadge?: boolean;
 }
 
 export const Avatar = (
-	{
-		src,
-		alt,
-		size = 40,
-		wrapperClassName = '',
-		imageClassName = '',
-		badge = {
-			model: 'simple',
-			anchorPoints: { vertical: 'bottom', horizontal: 'right' },
-			className: ''
-		}
-	}: AvatarProps
+		{
+				src,
+				alt,
+				size = 40,
+				wrapperClassName = '',
+				imageClassName = '',
+				badge = {
+						model: 'simple',
+						anchorPoints: { vertical: 'bottom', horizontal: 'right' },
+						className: ''
+				},
+				noBadge = false,
+		}: AvatarProps
 ) => {
 
-	const simpleBadgeClassnames = classnames(
-		{ [ styles[ 'avatar-simple-badge' ] ]: badge.model === 'simple' },
-		styles[ badge.anchorPoints!.horizontal ],
-		styles[ badge.anchorPoints!.vertical ],
-	)
+		const simpleBadgeClassnames = classnames(
+				{ [styles['avatar-simple-badge']]: badge.model === 'simple' && !noBadge },
+				styles[badge.anchorPoints!.horizontal],
+				styles[badge.anchorPoints!.vertical],
+		)
 
-	return (
-		<div className={
-			classnames(
-				styles[ 'avatar' ],
-				simpleBadgeClassnames,
-				wrapperClassName
-			)
-		}>
-			<Image
-				src={ src }
-				objectFit={ "cover" }
-				layout={ "intrinsic" }
-				alt={ alt }
-				width={ size }
-				height={ size }
-				className={
-					classnames(imageClassName)
-				}
-			/>
+		return (
+				<div className={
+						classnames(
+								styles['avatar'],
+								simpleBadgeClassnames,
+								wrapperClassName
+						)
+				}>
+						<Image
+								src={ src }
+								objectFit={ "cover" }
+								layout={ "intrinsic" }
+								alt={ alt }
+								width={ size }
+								height={ size }
+								className={
+										classnames(imageClassName)
+								}
+						/>
 
-			{ badge &&
-			RenderIf(badge.model === 'custom', (
-				<div className={ classnames(
-					badge.className!,
-					styles[ badge.anchorPoints!.horizontal ],
-					styles[ badge.anchorPoints!.vertical ],
-					styles[ 'badge' ],
-				) }>
-					{ badge.customComponent }
+						{ badge && !noBadge &&
+						RenderIf(badge.model === 'custom', (
+								<div className={ classnames(
+										badge.className!,
+										styles[badge.anchorPoints!.horizontal],
+										styles[badge.anchorPoints!.vertical],
+										styles['badge'],
+								) }>
+										{ badge.customComponent }
+								</div>
+						)) }
 				</div>
-			)) }
-		</div>
-	);
+		);
 }
